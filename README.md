926637e4a043671e0aed839ead377aa3d6b3af15
bragelbytes <85720959+bragelbytes@users.noreply.github.com>
2023-02-10 16:43:59 -0800

    Refactored to utilize package.swift

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wreath
+
+A description of this package.
---
+++ Tests/WreathTests/WreathTests.swift
@@ -0,0 +1,14 @@
+//import XCTest
+//
+//import Antiphony
+//
+//@testable import Wreath
+//
+//final class WreathTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(Wreath().text, "Hello, World!")
+//    }
+//}
