ca8c43c06a42c116e20e93af795ceddb53b4b402
consuelita <adelita@operatorfoundation.org>
2023-02-14 19:14:06 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Wreath",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "WreathClient",
             targets: ["WreathClient"])],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.4"),
-        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Antiphony", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Bootstrap.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", branch: "main")
     ],
     targets: [
         .target(name: "Wreath",
@@ -23,19 +24,20 @@ let package = Package(
                ]),
         .target(name: "WreathClient",
                dependencies: [
+                "Antiphony",
                 "Wreath",
-                .product(name: "Antiphony", package: "Antiphony"),
+                .product(name: "BootstrapClient", package: "Bootstrap")
                ]),
         .executableTarget(
             name: "WreathServer",
             dependencies: [
+                "Antiphony",
                 "Wreath",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Antiphony", package: "Antiphony"),
             ]),
         .testTarget(
             name: "WreathTests",
-            dependencies: []
+            dependencies: ["Wreath"]
         ),
     ]
 )
