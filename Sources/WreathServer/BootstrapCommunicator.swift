3375def712c61e4cf91793c905ed9dc7d1bb5d20
bragelbytes <85720959+bragelbytes@users.noreply.github.com>
2023-02-20 16:30:12 -0800

    Added test and broke out some functions in Bootstrap Communicator

--- Sources/WreathServer/BootstrapCommunicator.swift
+++ Sources/WreathServer/BootstrapCommunicator.swift
@@ -9,33 +9,57 @@ import Foundation
 
 import Arcadia
 import BootstrapClient
+import Gardener
 import TransmissionTypes
 
 class BootstrapCommunicator
 {
     let bootstrapClient: BootstrapClient
+    let serverID: String
+    let serverIP: String
+    let serverPort: Int
+    let bootstrapClientConfigFilename = "bootstrap-client.json"
     
-    init(connection: TransmissionTypes.Connection)
+    //TODO: Add a timer for send heartbeat
+    
+    init(serverID: String, serverIP: String, serverPort: Int, connection: TransmissionTypes.Connection)
     {
         self.bootstrapClient = BootstrapClient(connection: connection)
+        self.serverID = serverID
+        self.serverIP = serverIP
+        self.serverPort = serverPort
     }
     
     func someThingsThatBootstrapClientsDo() throws
     {
+        // TODO: Make a test for this and break it out into its own function.
         // Gets a list of other wreath servers
-        let listOfOtherWreathServers = try bootstrapClient.getAddresses(serverID: "fakeserverID")
+        let listOfOtherWreathServers = try bootstrapClient.getAddresses(serverID: self.serverID)
         print("Received a list of peers from the bootstrap server:")
         for wreathServer in listOfOtherWreathServers
         {
             print(wreathServer)
         }
-
-        // Registers the provided wreath server with the bootstrap server
-        let wreathServerInformation = WreathServerInfo(serverID: "fakeserverid", serverAddress: "someaddress")
-        try bootstrapClient.registerNewAddress(newServer: wreathServerInformation)
-
-
-         // A keepalive function that lets the Bootstrap server know that the given wreath server is still active
-        try bootstrapClient.sendHeartbeat(serverID: "fakeServerID")
+    }
+    
+    /// Registers the provided wreath server with the bootstrap server
+    func registerNewAddress() throws
+    {
+        print("Registering new address...")
+        let configURL = File.homeDirectory().appendingPathComponent(bootstrapClientConfigFilename)
+        let client = try BootstrapClient(configURL: configURL)
+        let serverInfo = WreathServerInfo(serverID: self.serverID, serverAddress: "\(serverIP):\(serverPort)")
+        try client.registerNewAddress(newServer: serverInfo)
+        print("New address registered!")
+    }
+    
+    /// A keepalive function that lets the Bootstrap server know that the given wreath server is still active
+    func sendHeartbeat() throws
+    {
+        print("Sending heartbeat...")
+        let configURL = File.homeDirectory().appendingPathComponent(bootstrapClientConfigFilename)
+        let client = try BootstrapClient(configURL: configURL)
+        try client.sendHeartbeat(serverID: self.serverID)
+        print("Heartbeat sent!")
     }
 }
--- Tests/WreathTests/WreathTests.swift
+++ Tests/WreathTests/WreathTests.swift
@@ -11,13 +11,23 @@ import TransmissionTypes
 
 final class WreathTests: XCTestCase {
     
+    
+    func testRegisterNewAddress() throws
+    {
+        print("Test registering new address...")
+        let configURL = File.homeDirectory().appendingPathComponent("bootstrap-client.json")
+        let client = try BootstrapClient(configURL: configURL)
+        let serverInfo = WreathServerInfo(serverID: "thisisnotarealid", serverAddress: "127.0.0.1:1234")
+        try client.registerNewAddress(newServer: serverInfo)
+        print("Test complete!")
+    }
+    
     func testHeartbeat() throws
     {
         print("Testing heartbeat...")
         let configURL = File.homeDirectory().appendingPathComponent("bootstrap-client.json")
         let client = try BootstrapClient(configURL: configURL)
-        let serverInfo = WreathServerInfo(serverID: "thisisnotarealid", serverAddress: "127.0.0.1:1234")
-        try client.registerNewAddress(newServer: serverInfo)
         try client.sendHeartbeat(serverID: "thisisnotarealid")
+        print("Test complete!")
     }
 }
